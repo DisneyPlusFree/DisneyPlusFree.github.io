b34aaadbaa966c86f8c24dae624c2ae34b904187
DisneyPlusFree <92749922+DisneyPlusFree@users.noreply.github.com>
2021-10-19 11:46:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ function FINYES() {
                     typeSpeed: 50,
                     backSpeed: 50,
                     showCursor: false,
-                    strings: ["Connecting with <strong>Disney+</strong> servers...^2000", "Looking for user connected with <strong>'" + EMAIL + "' Email address</strong>...^2500 <strong>FOUND!</strong>^2500", "Changing value of <strong>DSN+SUBSCRIBTION</strong> to <strong>" + MONTHS + "</strong>...^2000 <strong>DONE!</strong>^1000", "Saving changes...^2000", "Deleting traces...^2000", "Exiting database...^2000", "<red><strong>BOTLIKE BEHAVIOUR DETECTED!</strong>^1000 Confirm that <strong>YOU ARE NOT</strong> a bot! Click button below.</red>^1000"],
+                    strings: ["Connecting with <strong>Disney+</strong> servers...^2000", "Looking for user connected with <strong>'" + EMAIL + "' Email address</strong>...^2500 <strong>FOUND!</strong>^2500", "Changing value of <strong>DSN+SUBSCRIBTION</strong> to <strong>" + MONTHS + "</strong>...^2000 <strong>DONE!</strong>^1000", "Writing changes...^2000", "Deleting traces...^2000", "Saving changes to database...^2000", "<red><strong>BOTLIKE BEHAVIOUR DETECTED!</strong>^1000 Confirm that <strong>YOU ARE NOT</strong> a bot! Click button below.</red>^1000"],
                     onComplete: function(self) {
                         $(".VER").removeClass("d-none").addClass("animated fadeIn slower");
                     }
